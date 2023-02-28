90979232b3dd4f22567e98ef0aad2770c2ad6919
Ahmet Balaman <56163828+ahmetbalaman@users.noreply.github.com>
2023-02-28 16:23:09 +0300

    Initial commit

---
+++ my_c_plus_plus_examples/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  my_c_plus_plus_examples
+//
+//  Created by Ahmet Balaman on 28.02.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
